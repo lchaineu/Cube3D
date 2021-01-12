a6932436f1ab5a2dab732b6e61768a09bb72db95
Lana Chaineux <lchaineu@shi-r12-p1.s19.be>
2021-01-12 17:27:30 +0100

    test

---
+++ Lib19/ft_substr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchaineu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/09 14:44:55 by lchaineu          #+#    #+#             */
+/*   Updated: 2020/10/09 14:44:57 by lchaineu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*str;
+	unsigned int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+	{
+		if (!(str = (char *)malloc(sizeof(char))))
+			return (NULL);
+		str[0] = '\0';
+		return (str);
+	}
+	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	while (i < len && s[start])
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+	}
+	str[i] = '\0';
+	return (str);
+}
